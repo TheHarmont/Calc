01d2160cf0b7c4262f55381c696ad76c76d8f75f
Margarita <rita30364@gmail.com>
2020-05-30 15:37:45 +1000

    Finish

--- HelloWORLD/Source.cpp
+++ HelloWORLD/Source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
- cout << "Hello World!";
+	int a;
+	cin >> a;
+	if (a > 4) {
+		cout << "Hello World!";
+		cout << "Yes, my loard";
+	}
+	else cout << "Fu** off my little baby, and goodbye";
 }
